6bca8d7abe93a3f14513c2da033b7c837cf227d3
Ganapati Tippa <ganapatit@exeire.com>
2025-11-14 19:02:06 +0530

    removed cookie utilities

--- cart.js
+++ cart.js
@@ -1,29 +1,5 @@
 (function () {
-    // --- COOKIE UTILITIES ---
-    window.setCookie = (name, value, days = 7, domain = '.kwilthealth.com') => {
-        let expires = "";
-        if (days) {
-            const date = new Date();
-            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-            expires = "; expires=" + date.toUTCString();
-        }
-        document.cookie = name + "=" + (value || "") + expires + "; path=/; domain=" + domain + "; SameSite=Lax; Secure";
-    };
-
-    window.getCookie = (name) => {
-        const nameEQ = name + "=";
-        const ca = document.cookie.split(';');
-        for (let i = 0; i < ca.length; i++) {
-            let c = ca[i];
-            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-        }
-        return null;
-    };
-
-    window.deleteCookie = (name, domain = '.kwilthealth.com') => {
-        document.cookie = name + '=; Max-Age=-99999999; path=/; domain=' + domain;
-    };
+    
 
     // --- API CONFIGURATION ---
     const API_BASE_URL = 'https://kwilt-order-396730550724.us-central1.run.app';
